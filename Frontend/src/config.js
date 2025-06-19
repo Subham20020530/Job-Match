3abcc411d65f27e84eb98d2e0fe2f30b8e0f799c
SUBHAM KUNDU <KUNDUSUBHAM801@GMAIL.COM>
2025-06-20 00:17:56 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://job-match-yjr1.onrender.com';
 export const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL || 'https://api.cloudinary.com/v1_1/subham2002/image/upload';
 export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET || 'JobMatch';
 export const CLOUDINARY_CLOUD_NAME = 'subham2002';
